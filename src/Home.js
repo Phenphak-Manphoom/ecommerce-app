cba3bd40e40d2ee436e6a2f2dd3b7a2b72f09993
phenphak <phenphak.forlearn@gmail.com>
2023-07-28 18:19:20 +0700

    styled components for styling react app

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+  return <Wrapper className="test">Home</Wrapper>;
 }
 
-export default Home
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.bg};
+  width: 20rem;
+  height: 20rem;
+`;
+export default Home;
