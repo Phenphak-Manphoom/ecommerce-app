e9763aca6fcdd0c93b284407311c795066bc3f98
phenphak <phenphak.forlearn@gmail.com>
2023-08-07 21:32:59 +0700

    feat:add service and trusted feature

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
-
 import HeroSection from "./components/HeroSection";
-
+import Service from "./components/Service";
+import Trusted from "./components/Trusted";
 function Home() {
   const data = {
     name: "Thapa Store",
   };
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Service />
+      <Trusted />
+    </>
+  );
 }
 
 export default Home;
